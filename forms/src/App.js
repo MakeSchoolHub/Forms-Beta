d4c44810cf61f171c9fe4bf2e694c93e31ac4487
Muhammad Tifak <moht20009@gmail.com>
2020-10-04 16:36:43 -0400

    Finished basic todo gaming tags app

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 //Here we're importing components
@@ -8,13 +8,38 @@ import TodoList from "./components/TodoList"
 function App() {
   const [inputText, setInputText] = useState("")
   const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState('all')
+  const [filteredTodos, setFilteredTodos] = useState([])
+
+  //effect
+  useEffect(() => {
+    filterHandler()
+  }, [todos, status])
+
+  //functions
+  const filterHandler = () => {
+    switch(status){
+      case "completed":
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break
+      case "uncompleted":
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break
+      default:
+        setFilteredTodos(todos)
+        break
+    }
+  }
+
+  //save to local, just an easy way without using databases
+
   return (
     <div className="App">
       <header>
       <h1>BONDfyr Gaming Tags</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus} />
+      <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos} />
     </div>
   );
 }
